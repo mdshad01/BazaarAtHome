e5ebe5fface8404f1c0481ff3387df863b0b2a5a
Shad <shadalam0024@gmail.com>
2025-05-22 17:07:46 +0530

    User Structure is created

---
+++ server/models/User.js
@@ -0,0 +1,15 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+	{
+		name: { type: String, required: true },
+		email: { type: String, required: true, unique: true },
+		password: { type: String, required: true },
+		cartItems: { type: Object, default: {} },
+	},
+	{ minimize: false }
+);
+
+const User = mongoose.model("user", userSchema);
+
+export default User;
