123c1066dec92a43cd6ba282e6a855358b85129f
Shad <shadalam0024@gmail.com>
2025-05-23 18:11:54 +0530

    user controller or route work perfectly

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,8 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-export default register = async (req, res) => {
+// Register User : /api/user/register
+export const register = async (req, res) => {
 	try {
 		const { name, email, password } = req.body;
 		if (!name || !email || !password) {
@@ -26,6 +27,50 @@ export default register = async (req, res) => {
 			secure: process.env.NODE_ENV === "production", //Use secure cookies in production
 			sameSite: process.env.NODE_ENV === "production" ? "none" : "strict", //CSRF Protection
 			maxAge: 7 * 24 * 60 * 60 * 1000, // cookie expiration tiome
+		});
+		return res.json({
+			success: true,
+			user: { email: user.email, name: user.name },
+		});
+	} catch (error) {
+		console.log(error.message);
+		res.json({ success: false, message: error.message });
+	}
+};
+
+// Login User : /api/user/login
+
+export const login = async (req, res) => {
+	try {
+		let { email, password } = req.body;
+		if (!email || !password) {
+			return res.json({
+				success: false,
+				message: "Email and Password are required",
+			});
+		}
+		const user = await User.findOne({ email });
+		if (!user) {
+			return res.json({
+				success: false,
+				message: "Invalid Email or Password",
+			});
+		}
+		const isMatch = await bcrypt.compare(password, user.password);
+		if (!isMatch) {
+			return res.json({
+				success: false,
+				message: "Invalid Email or Password",
+			});
+		}
+		const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+			expiresIn: "7d",
+		});
+		res.cookie("token", token, {
+			httpOnly: true,
+			secure: process.env.NODE_ENV === "production",
+			sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+			maxAge: 7 * 24 * 60 * 60 * 1000,
 		});
 		return res.json({
 			success: true,
