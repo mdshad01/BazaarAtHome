ab129acc6e1ce859741563cbcb7a0e4570281a40
Shad <shadalam0024@gmail.com>
2025-05-24 23:26:19 +0530

    cloudinary and multer initilized  and appProuct function is creaded

---
+++ server/controllers/productController.js
@@ -0,0 +1,34 @@
+// Add Product : /api/product/add
+
+export const addProduct = async (req, res) => {
+	try {
+		let productData = JSON.parse(req.body.productData);
+
+		const images = req.files;
+
+		let imagesUrl = await Promise.all(
+			images.map(async (item) => {
+				let result = await cloudinary.uploader.upload(item.path, {
+					resource_type: "image",
+				});
+				return result.secure_url;
+			})
+		);
+
+		await Product.create({ ...productData, image: imagesUrl });
+		return res.json({ success: true, message: "Product Added" });
+	} catch (error) {
+		console.log(error.message);
+		res.json({ success: false, message: error.message });
+	}
+};
+
+// Get Product : /api/product/list
+
+export const productList = async (req, res) => {};
+// Get single Product : /api/product/id
+
+export const productById = async (req, res) => {};
+// Change product inStock : /api/product/stock
+
+export const changeStock = async (req, res) => {};
---
+++ server/models/Product.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+	{
+		name: { type: String, required: true },
+		description: { type: Array, required: true },
+		price: { type: Number, required: true },
+		offerPrice: { type: Number, required: true },
+		image: { type: Array, required: true },
+		category: { type: Array, required: true },
+		inStock: { type: Boolean, default: true },
+		cartItems: { type: Object, default: {} },
+	},
+	{ timestamps: true }
+);
+
+const Product =
+	mongoose.models.product || mongoose.model("product", productSchema);
+
+export default Product;
