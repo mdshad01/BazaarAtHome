ace9e8cf1fa274b07cefbff9deba6c14d7d50e66
Shad <shadalam0024@gmail.com>
2025-07-29 15:31:23 +0530

    bug fix

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ await connectDB();
 await connectCloudinary();
 
 // Allow multiple origins
-const allowedOrigins = ["http://localhost:5173", "https://bazaar-at-home-backend.vercel.app"];
+const allowedOrigins = ["http://localhost:5173", "https://bazaar-at-home.vercel.app"];
 
 app.post("/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
 
