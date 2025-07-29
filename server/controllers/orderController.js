3bc40e8d640bb7f30112c95e5c9366309e244e9d
Shad <shadalam0024@gmail.com>
2025-07-29 11:25:23 +0530

    bug fix

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -123,7 +123,7 @@ export const stripeWebhooks = async (request, response) => {
   let event;
 
   try {
-    event = stripeInstance.webhooks.constructEvent(request.body, sug, process.env.STRIPE_WEBHOOK_SECRET);
+    event = stripeInstance.webhooks.constructEvent(request.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
   } catch (error) {
     response.status(400).send(`Webhook Error : ${error.message}`);
   }
